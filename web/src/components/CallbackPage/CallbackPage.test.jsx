77eb5de5e13dbfaaa08bcfe083c6f19068e60612
Jacob Sowles <jrsowles@gmail.com>
2018-11-19 20:36:43 -0800

    add CallbackPage tests

--- web/src/components/CallbackPage/CallbackPage.test.jsx
+++ web/src/components/CallbackPage/CallbackPage.test.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
+import { init } from '@tests/test-base';
 import CallbackPage from './CallbackPage';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<CallbackPage />, div);
+describe('CallbackPage', () => {
+  init();
+  let props;
+
+  const component = () => {
+    return mount(<CallbackPage {...props} />);
+  };
+
+  const topLevelElement = () => {
+    return component()
+      .find('div')
+      .first();
+  };
+
+  beforeEach(() => {
+    props = {
+      className: undefined
+    };
+  });
+
+  it('should render a div element', () => {
+    topLevelElement().should.not.equal(undefined);
+  });
+
+  describe('the rendered div element', () => {
+    it('should always render an image element', () => {
+      component()
+        .find('img')
+        .should.have.lengthOf(1);
+    });
+  });
 });
