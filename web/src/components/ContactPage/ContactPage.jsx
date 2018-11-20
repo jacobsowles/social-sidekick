3876ecddfde61b18f8c9c74d4d973c27d0d69cfc
Jacob Sowles <jrsowles@gmail.com>
2018-11-20 15:14:54 -0800

    add ContactPage tests

--- web/src/components/ContactPage/ContactPage.jsx
+++ web/src/components/ContactPage/ContactPage.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import Page from '@components/Page';
 import './ContactPage.scss';
 
-const ContactPage = () => {
+const ContactPage = ({ className, ...rest }) => {
   return (
-    <Page className="contact-page" title="Contact">
+    <Page className={classNames('contact-page', className)} title="Contact" {...rest}>
       <p>Page content</p>
     </Page>
   );
+};
+
+ContactPage.propTypes = {
+  className: PropTypes.string
+};
+
+ContactPage.defaultProps = {
+  className: undefined
 };
 
 export default ContactPage;
--- web/src/components/ContactPage/ContactPage.test.jsx
+++ web/src/components/ContactPage/ContactPage.test.jsx
@@ -1,8 +1,64 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow as enzymeShallow } from 'enzyme';
+import { init } from '@tests/test-base';
 import ContactPage from './ContactPage';
+import Page from '../Page';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<ContactPage />, div);
+describe('ContactPage', () => {
+  init();
+  let props;
+
+  const shallow = () => {
+    return enzymeShallow(<ContactPage {...props} />);
+  };
+
+  const topLevelElement = () => {
+    return shallow()
+      .find(Page)
+      .first();
+  };
+
+  beforeEach(() => {
+    props = {
+      className: undefined
+    };
+  });
+
+  it('should render a Page component', () => {
+    shallow()
+      .find(Page)
+      .should.have.lengthOf(1);
+  });
+
+  it('should pass `title` to the Page component as a prop', () => {
+    topLevelElement()
+      .props()
+      .title.should.not.equal(undefined);
+  });
+
+  describe('when `className` is defined', () => {
+    beforeEach(() => {
+      props.className = 'another-class';
+    });
+
+    it('should include the specified class name', () => {
+      topLevelElement()
+        .props()
+        .className.should.contain('another-class');
+    });
+
+    it('should include the contact-page class name', () => {
+      topLevelElement()
+        .props()
+        .className.should.contain('contact-page');
+    });
+  });
+
+  describe('when `className` is undefined', () => {
+    it('should include the contact-page class name', () => {
+      topLevelElement()
+        .props()
+        .className.should.contain('contact-page');
+    });
+  });
 });
