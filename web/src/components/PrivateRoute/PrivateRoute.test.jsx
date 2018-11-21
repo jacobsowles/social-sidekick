292c31365449c20867bef5eca6b000669cb3aed2
Jacob Sowles <jrsowles@gmail.com>
2018-11-21 11:17:06 -0800

    add PrivateRoute tests

--- web/src/components/PrivateRoute/PrivateRoute.test.jsx
+++ web/src/components/PrivateRoute/PrivateRoute.test.jsx
@@ -1,8 +1,49 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { Route } from 'react-router-dom';
+import { MemoryRouter } from 'react-router';
+import { init, mount } from '@tests/test-base';
 import PrivateRoute from './PrivateRoute';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<PrivateRoute />, div);
+describe('PrivateRoute', () => {
+  let props;
+  init(
+    () => (
+      <MemoryRouter>
+        <PrivateRoute {...props} />
+      </MemoryRouter>
+    ),
+    Route
+  );
+
+  beforeEach(() => {
+    props = {
+      component: () => <div />,
+      isAuthenticated: true,
+      login: jest.fn()
+    };
+  });
+
+  it('should always render a Route component', () => {
+    mount()
+      .find(Route)
+      .should.have.lengthOf(1);
+  });
+
+  describe('when `isAuthenticated` is true', () => {
+    it('should not call `login`', () => {
+      mount();
+      expect(props.login).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('when `isAuthenticated` is false', () => {
+    beforeEach(() => {
+      props.isAuthenticated = false;
+    });
+
+    it('should call `login`', () => {
+      mount();
+      expect(props.login).toHaveBeenCalled();
+    });
+  });
 });
