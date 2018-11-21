6c3272158f72c29cf410805978054a593f37749b
Jacob Sowles <jrsowles@gmail.com>
2018-11-20 17:20:48 -0800

    refactor CallbackPage tests with new test base functions

--- web/src/components/CallbackPage/CallbackPage.test.jsx
+++ web/src/components/CallbackPage/CallbackPage.test.jsx
@@ -1,35 +1,19 @@
 import React from 'react';
-import { mount } from 'enzyme';
-import { init } from '@tests/test-base';
+import { init, mount, shallow } from '@tests/test-base';
 import CallbackPage from './CallbackPage';
 
 describe('CallbackPage', () => {
-  init();
-  let props;
-
-  const component = () => {
-    return mount(<CallbackPage {...props} />);
-  };
-
-  const topLevelElement = () => {
-    return component()
-      .find('div')
-      .first();
-  };
-
-  beforeEach(() => {
-    props = {
-      className: undefined
-    };
-  });
+  init(() => <CallbackPage />);
 
   it('should render a div element', () => {
-    topLevelElement().should.not.equal(undefined);
+    shallow()
+      .find('div')
+      .should.have.lengthOf(1);
   });
 
   describe('the rendered div element', () => {
     it('should always render an image element', () => {
-      component()
+      mount()
         .find('img')
         .should.have.lengthOf(1);
     });
