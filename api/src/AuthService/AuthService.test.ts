96f7f0b444ec5fa0da06cf9a9272c772f34367dc
Jacob Sowles <jrsowles@gmail.com>
2018-11-26 11:26:55 -0800

    git add more AuthService tests and switch to moment.js for date handling

--- api/src/AuthService/AuthService.test.ts
+++ api/src/AuthService/AuthService.test.ts
@@ -1,5 +1,6 @@
 import { init, service } from '../tests/test-base';
 import AuthService from './AuthService';
+import moment from 'moment';
 
 jest.mock('../History/history', () => {
   return {
@@ -9,6 +10,52 @@ jest.mock('../History/history', () => {
 
 describe('AuthService', () => {
   init(AuthService);
+
+  beforeEach(() => {
+    localStorage.clear();
+
+    expect(localStorage.getItem('access_token')).toBe(null);
+    expect(localStorage.getItem('expires_at')).toBe(null);
+    expect(localStorage.getItem('id_token')).toBe(null);
+  });
+
+  describe('isAuthenticated', () => {
+    it('should return true when `expires_at` local storage value is in the future', () => {
+      localStorage.setItem(
+        'expires_at',
+        moment()
+          .add('1', 'm')
+          .valueOf()
+          .toString()
+      );
+      service.isAuthenticated().should.equal(true);
+    });
+
+    it('should return false when `expires_at` local storage value is right now', () => {
+      localStorage.setItem(
+        'expires_at',
+        moment()
+          .valueOf()
+          .toString()
+      );
+      service.isAuthenticated().should.equal(false);
+    });
+
+    it('should return false when `expires_at` local storage value is in the past', () => {
+      localStorage.setItem(
+        'expires_at',
+        moment()
+          .subtract('1', 'm')
+          .valueOf()
+          .toString()
+      );
+      service.isAuthenticated().should.equal(false);
+    });
+
+    it('should return false when `expires_at` local storage value is not set', () => {
+      service.isAuthenticated().should.equal(false);
+    });
+  });
 
   describe('logout', () => {
     beforeEach(() => {
@@ -29,6 +76,30 @@ describe('AuthService', () => {
 
     it('should remove `expires_at` from local storage', () => {
       expect(localStorage.getItem('expires_at')).toBe(null);
+    });
+  });
+
+  describe('setSession', () => {
+    const authResult = {
+      accessToken: 'Access Token',
+      expiresIn: '100',
+      idToken: 'ID Token'
+    };
+
+    beforeEach(() => {
+      service.setSession(authResult);
+    });
+
+    it('should set `access_token` in local storage', () => {
+      expect(localStorage.getItem('access_token')).toBe('Access Token');
+    });
+
+    it('should set `expires_at` in local storage', () => {
+      expect(localStorage.getItem('expires_at')).not.toBe(null);
+    });
+
+    it('should set `id_token` in local storage', () => {
+      expect(localStorage.getItem('id_token')).toBe('ID Token');
     });
   });
 });
