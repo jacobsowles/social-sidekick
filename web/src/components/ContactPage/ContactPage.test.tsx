b0838fdd6be5ea9457bdad71fb67d419a8b27f54
Jacob Sowles <jrsowles@gmail.com>
2018-12-05 06:59:57 -0800

    remove className prop and add tests for ContactPage

---
+++ web/src/components/ContactPage/ContactPage.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { init, shallow, shallowTopLevelElement } from '@tests/component-test-base';
+import ContactPage from '@components/ContactPage';
+
+describe('ContactPage', () => {
+  init(() => <ContactPage />, 'div');
+
+  it('should always render a div element', () => {
+    shallow()
+      .find('div')
+      .should.have.lengthOf(1);
+  });
+
+  it('should only have the default class name', () => {
+    shallowTopLevelElement()
+      .prop('className')
+      .should.equal('contact-page');
+  });
+});
--- web/src/components/ContactPage/ContactPage.tsx
+++ web/src/components/ContactPage/ContactPage.tsx
@@ -1,27 +1,18 @@
-import classNames from 'classnames';
 import React, { FunctionComponent } from 'react';
 
 import ContentBox from '@components/ContentBox';
 import PageHeader from '@components/PageHeader';
 import './ContactPage.scss';
 
-export interface ContactPageProps {
-  className?: string;
-}
-
-const ContactPage: FunctionComponent<ContactPageProps> = ({ className }) => {
+const ContactPage: FunctionComponent = () => {
   return (
-    <div className={classNames('contact-page', className)}>
+    <div className="contact-page">
       <ContentBox>
         <PageHeader title="Contact" />
         <p>Page content</p>
       </ContentBox>
     </div>
   );
-};
-
-ContactPage.defaultProps = {
-  className: undefined
 };
 
 export default ContactPage;
