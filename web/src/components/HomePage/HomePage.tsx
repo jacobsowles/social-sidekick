42189661392eb49d8513ba0b7c111a11b942cfe6
Jacob Sowles <jrsowles@gmail.com>
2018-12-05 07:22:05 -0800

    remove HomePage className prop and add tests

--- web/src/components/HomePage/HomePage.tsx
+++ web/src/components/HomePage/HomePage.tsx
@@ -1,27 +1,18 @@
-import classNames from 'classnames';
 import React from 'react';
 
 import ContentBox from '@components/ContentBox';
 import PageHeader from '@components/PageHeader';
 import './HomePage.scss';
 
-interface HomePageProps {
-  className?: string;
-}
-
-const HomePage: React.FunctionComponent<HomePageProps> = ({ className }) => {
+const HomePage: React.FunctionComponent = () => {
   return (
-    <div className={classNames('home-page', className)}>
+    <div className="home-page">
       <ContentBox>
         <PageHeader title="Home" />
         <p>Page content</p>
       </ContentBox>
     </div>
   );
-};
-
-HomePage.defaultProps = {
-  className: undefined
 };
 
 export default HomePage;
