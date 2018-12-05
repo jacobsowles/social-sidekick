b82d50806180287b7fbe679d720ae4bb9fa873a7
Jacob Sowles <jrsowles@gmail.com>
2018-12-05 12:11:21 -0800

    test: add App and PostAuthPage test skeletons

--- web/src/components/App/App.test.tsx
+++ web/src/components/App/App.test.tsx
@@ -4,38 +4,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { createStore } from 'redux';
 
 import rootReducer from '@reducers/index';
-import { init, render, shallow, shallowTopLevelElement } from '@tests/component-test-base';
+import { init } from '@tests/component-test-base';
 import App from './App';
 
 describe('App', () => {
   const store = createStore(rootReducer);
 
-  init(
-    () => (
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    ),
-    'div'
-  );
-
-  it('should always render a div element', () => {
-    shallow()
-      .find('div')
-      .should.have.lengthOf(1);
-  });
-
-  it('should always render a footer element', () => {
-    render()
-      .find('footer')
-      .should.have.lengthOf(1);
-  });
-
-  it('should always include the default class names', () => {
-    shallowTopLevelElement()
-      .prop('className')
-      .should.equal('app');
-  });
+  init(() => (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  ));
 });
---
+++ web/src/components/PostAuthPage/PostAuthPage.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { init } from '@tests/component-test-base';
+import PostAuthPage from '@components/PostAuthPage';
+
+describe('PostAuthPage', () => {
+  init(() => <PostAuthPage />);
+});
