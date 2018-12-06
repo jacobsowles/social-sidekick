43664d3d1948f0f5ed5861d49d9129a6bfadb98d
Jacob Sowles <jrsowles@gmail.com>
2018-12-05 19:23:14 -0800

    feat: add LoadingSpinner component

---
+++ web/src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,35 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
+import React, { FunctionComponent } from 'react';
+
+import './LoadingSpinner.scss';
+
+export interface LoadingSpinnerProps {
+  className?: string;
+  size?:
+    | 'xs'
+    | 'sm'
+    | 'lg'
+    | '1x'
+    | '2x'
+    | '3x'
+    | '4x'
+    | '5x'
+    | '6x'
+    | '7x'
+    | '8x'
+    | '9x'
+    | '10x'
+    | undefined;
+}
+
+const LoadingSpinner: FunctionComponent<LoadingSpinnerProps> = ({ className, size, ...rest }) => {
+  return <FontAwesomeIcon icon={['fas', 'spinner']} size={size} {...rest} spin />;
+};
+
+LoadingSpinner.defaultProps = {
+  className: undefined,
+  size: '4x'
+};
+
+export default LoadingSpinner;
--- web/src/core/icons/icons.ts
+++ web/src/core/icons/icons.ts
@@ -1,8 +1,8 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faCube, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faCube, faSignOutAlt, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 export const init = (): void => {
-  library.add(faCube, faGithub, faQuestionCircle, faSignOutAlt);
+  library.add(faCube, faGithub, faQuestionCircle, faSignOutAlt, faSpinner);
 };
