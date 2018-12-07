8590a5b6de98c56562e503431d45aabdfdddf12a
Jacob Sowles <jrsowles@gmail.com>
2018-12-06 16:47:21 -0800

    feat(Connections): add loading spinner

--- web/src/components/ConnectionsPage/ConnectionsPage.tsx
+++ web/src/components/ConnectionsPage/ConnectionsPage.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames';
 import React, { FunctionComponent } from 'react';
 
 import ContentBox from '@components/ContentBox';
+import LoadingSpinner from '@components/LoadingSpinner';
 import PageHeader from '@components/PageHeader';
 import ServiceIcon from '@components/ServiceIcon';
 import { Service } from '@core/types';
@@ -20,9 +21,11 @@ const ConnectionsPage: FunctionComponent<ConnectionsPageProps> = ({ allServices,
           title="Connect Services"
           subtitle="Log into the services below to connect them to your Social Sidekick account."
         />
-        {allServices.map((service: Service, key: number) => (
-          <ServiceIcon iconName={service.name.toLowerCase()} key={key} label={service.name} />
-        ))}
+        {(!allServices || allServices.length === 0) && <LoadingSpinner />}
+        {allServices &&
+          allServices.map((service: Service, key: number) => (
+            <ServiceIcon iconName={service.name.toLowerCase()} key={key} label={service.name} />
+          ))}
       </ContentBox>
     </main>
   );
