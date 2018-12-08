5af7390f551291c30ca4163b3caadf30db90db1c
Jacob Sowles <jrsowles@gmail.com>
2018-12-08 07:07:05 -0800

    style(ServiceIcon): add hover state styles

--- web/src/components/ServiceIcon/ServiceIcon.tsx
+++ web/src/components/ServiceIcon/ServiceIcon.tsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames';
-import React, { FunctionComponent } from 'react';
+import React, { Component } from 'react';
 
 import './ServiceIcon.scss';
 
@@ -26,57 +26,84 @@ export interface ServiceIconProps {
     | undefined;
 }
 
-const ServiceIcon: FunctionComponent<ServiceIconProps> = ({
-  children,
-  className,
-  iconName,
-  isConnected,
-  label,
-  size,
-  ...rest
-}) => {
-  const icons: IconProp[] = [
-    { iconName: 'github', prefix: 'fab' },
-    { iconName: 'twitter', prefix: 'fab' }
-  ];
-  let selectedIcon = icons.find(i => i.iconName === iconName);
-
-  if (!selectedIcon) {
-    selectedIcon = {
-      iconName: 'question-circle',
-      prefix: 'far'
-    };
-  }
-
-  const classes = classNames(
-    'service-icon',
-    { 'service-icon-connected': isConnected },
-    'service-icon-' + selectedIcon.iconName,
-    className
-  );
-
-  return (
-    <div className={classes}>
-      {isConnected && (
-        <span className="badge">
-          <FontAwesomeIcon icon="check" />
-        </span>
-      )}
-      <FontAwesomeIcon icon={[selectedIcon.prefix, selectedIcon.iconName]} size={size} {...rest} />
-      {label && <p className="service-icon-label">{label}</p>}
-    </div>
-  );
-};
-
-ServiceIcon.defaultProps = {
-  className: undefined,
-  label: undefined,
-  size: '4x'
-};
+export interface ServiceIconState {
+  isHover: boolean;
+}
 
 interface IconProp {
   prefix: any;
   iconName: any;
+}
+
+class ServiceIcon extends Component<ServiceIconProps, ServiceIconState> {
+  public static defaultProps = {
+    className: undefined as string,
+    label: undefined as string,
+    size: '4x'
+  };
+
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      isHover: false
+    };
+  }
+
+  public render() {
+    const { className, iconName, isConnected, label, size, ...rest } = this.props;
+
+    const icons: IconProp[] = [
+      { iconName: 'github', prefix: 'fab' },
+      { iconName: 'twitter', prefix: 'fab' }
+    ];
+
+    let selectedIcon = icons.find(i => i.iconName === iconName);
+
+    if (!selectedIcon) {
+      selectedIcon = {
+        iconName: 'question-circle',
+        prefix: 'far'
+      };
+    }
+
+    const classes = classNames(
+      'service-icon',
+      { 'service-icon-connected': isConnected },
+      'service-icon-' + selectedIcon.iconName,
+      this.props.className
+    );
+
+    return (
+      <div
+        className={classes}
+        onMouseOut={() =>
+          this.setState({
+            isHover: false
+          })
+        }
+        onMouseOver={() => this.setState({ isHover: true })}
+      >
+        {isConnected && (
+          <span className="badge">
+            {this.state.isHover ? (
+              <FontAwesomeIcon icon="times" />
+            ) : (
+              <FontAwesomeIcon icon="check" />
+            )}
+          </span>
+        )}
+
+        <FontAwesomeIcon
+          icon={[selectedIcon.prefix, selectedIcon.iconName]}
+          size={size}
+          {...rest}
+        />
+
+        {label && <p className="service-icon-label">{label}</p>}
+      </div>
+    );
+  }
 }
 
 export default ServiceIcon;
--- web/src/core/icons/icons.ts
+++ web/src/core/icons/icons.ts
@@ -1,8 +1,23 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons';
 import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faCheck, faCube, faSignOutAlt, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCheck,
+  faCube,
+  faSignOutAlt,
+  faSpinner,
+  faTimes
+} from '@fortawesome/free-solid-svg-icons';
 
 export const init = (): void => {
-  library.add(faCheck, faCube, faGithub, faQuestionCircle, faSignOutAlt, faSpinner, faTwitter);
+  library.add(
+    faCheck,
+    faCube,
+    faGithub,
+    faQuestionCircle,
+    faSignOutAlt,
+    faSpinner,
+    faTimes,
+    faTwitter
+  );
 };
