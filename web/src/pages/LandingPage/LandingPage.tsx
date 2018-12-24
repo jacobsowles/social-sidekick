d0dabdabcdb1ffcaa7760dcc6fef8d5fbb87fe88
Jacob Sowles <jrsowles@gmail.com>
2018-12-23 18:48:39 -0800

    feat(LandingPage): add copy

--- web/src/pages/LandingPage/LandingPage.tsx
+++ web/src/pages/LandingPage/LandingPage.tsx
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 import ContentBox from '@components/ContentBox';
@@ -8,8 +9,18 @@ const LandingPage: React.FunctionComponent = () => {
   return (
     <div className="landing-page">
       <ContentBox>
-        <PageHeader title="Landing Page" subtitle="Page subtitle" />
-        <p>Page content</p>
+        <PageHeader title="All of your profiles in one place" />
+        <p>
+          New job? Congratulations! But now you're faced with the laborious task of updating your
+          employer on all of your social networks.
+        </p>
+        <p>Looking to revamp your personal brand? Time to update all of your bios.</p>
+        <p>
+          Life changes fast and often. No more blocking off half an hour every time you need to
+          update your profiles. No more digging through menus trying to remember where the settings
+          are. No more forgetting to update certain profiles. Just sign up, connect your accounts,
+          and reclaim your time with Social Sidekick.
+        </p>
       </ContentBox>
     </div>
   );
